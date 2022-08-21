a873d14c8487a61b0d245335070c49174dcfb46a
Giovanni Emanuale Nocco <giovanni.nocco@gmail.com>
2022-08-21 15:11:56 +0200

    First implementation grpc

---
+++ src/05-saga-advanced/Genocs.MassTransit.GRPC.Client.Service/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+using Helloworld;
+
+Channel channel = new Channel("127.0.0.1:30051", ChannelCredentials.Insecure);
+
+var client = new Greeter.GreeterClient(channel);
+String user = "you";
+
+var reply = client.SayHello(new HelloRequest { Name = user });
+Console.WriteLine("Greeting: " + reply.Message);
+
+channel.ShutdownAsync().Wait();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
